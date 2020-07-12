d3cd571440e10964d8556119d9ddd67a5baa5541
Jiri Malek <jirka.malek@gmail.com>
2020-07-12 20:30:22 +0200

    Remove reference to unused SafeERC20 lib.

--- contracts/FusdToken.sol
+++ contracts/FusdToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
